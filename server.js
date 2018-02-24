47363f59ddcb781c74107df1cecacc41a7135cc4
“kalpesh6295” <kalpesh6295@gmail.com>
2018-02-25 01:29:01 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
+// const fs = require('fs')
+
 app = express();
+const port = process.env.port || 3000;
 app.use(express.static(__dirname+'/public'));
 hbs.registerPartials(__dirname+'/views/partials');
 app.get('/',(req,res)=>{
@@ -9,11 +12,14 @@ app.get('/',(req,res)=>{
         currentYear: '2018'
     });
 });
-app.get('/about',(req,res)=>{
-    res.render('about.hbs',{
-        pageTitle:'about page',
-        currentYear: '2018'
-    });
-});
+// app.get('/about',(req,res)=>{
+//     res.render('about.hbs',{
+//         pageTitle:'about page',
+//         currentYear: '2018'
+//     });
+// });
 
-app.listen(3000);
+
+app.listen(port,()=>{
+    console.log(port);
+});
