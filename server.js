fcc0b699e7530b933d6942aca74217e055c9f951
“kalpesh6295” <kalpesh6295@gmail.com>
2018-02-25 01:38:58 +0530

    server.js changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const hbs = require('hbs');
 // const fs = require('fs')
 
 app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static(__dirname+'/public'));
 hbs.registerPartials(__dirname+'/views/partials');
 app.get('/',(req,res)=>{
